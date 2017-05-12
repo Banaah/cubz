1f2dd867b77d8f60596fbf7dcf6919f48a10f29c
Banaah <nouvel.quentin@gmail.com>
2017-05-12 19:53:04 +0200

    Add files via upload

---
+++ test_amp.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
+led = [19,26,16,20]
+for i in led:
+    GPIO.setup(i, GPIO.HIGH)
+GPIO.output(20, GPIO.LOW)
+GPIO.output(16, GPIO.LOW)
+
+for j in range(100):
+    GPIO.output(26, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(26, GPIO.LOW)
+    GPIO.output(19, GPIO.HIGH)
+    time.sleep(0.05)
+    GPIO.output(19, GPIO.LOW)
+GPIO.cleanup()
