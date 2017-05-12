92f0eb5632d61ce20ce7ab59df2724708503fca8
Banaah <nouvel.quentin@gmail.com>
2017-05-12 20:58:31 +0200

    W

--- test_amp.py
+++ test_amp.py
@@ -4,14 +4,14 @@ GPIO.setmode(GPIO.BCM)
 led = [19,26,16,20]
 for i in led:
     GPIO.setup(i, GPIO.OUT)
-GPIO.output(20, GPIO.LOW)
-GPIO.output(16, GPIO.LOW)
+GPIO.output(20, GPIO.HIGH)
+GPIO.output(16, GPIO.HIGH)
 
 for j in range(100):
-    GPIO.output(26, GPIO.HIGH)
+    GPIO.output(20, GPIO.LOW)
     time.sleep(0.05)
-    GPIO.output(26, GPIO.LOW)
-    GPIO.output(19, GPIO.HIGH)
+    GPIO.output(20, GPIO.HIGH)
+    GPIO.output(16, GPIO.LOW)
     time.sleep(0.05)
-    GPIO.output(19, GPIO.LOW)
+    GPIO.output(16, GPIO.HIGH)
 GPIO.cleanup()
