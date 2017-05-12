d815fef3a5667d14938d9937b6d83be9dc11bb16
Banaah <nouvel.quentin@gmail.com>
2017-05-13 00:01:14 +0200

    :)

--- full_cube.py
+++ full_cube.py
@@ -7,6 +7,8 @@ layer = [27,18,17]
 pos = [5,6,12,13,19,16,21,20,26]
 for l in layer:
     GPIO.output(l,low)
+for l in pos:
+    GPIO.output(l,low)
 for i in range(100):
     for led in pos:
         GPIO.output(led,high)
