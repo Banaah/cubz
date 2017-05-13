18edeaaf04f46bcfcc00f86049a31876531c055b
Banaah <nouvel.quentin@gmail.com>
2017-05-13 18:23:51 +0200

    sdcsdd

--- test_amp.py
+++ test_amp.py
@@ -6,7 +6,7 @@ for i in led:
     GPIO.setup(i, GPIO.OUT)
 #GPIO.output(26, GPIO.HIGH)
 GPIO.output(17, GPIO.LOW)
-for i in range(10000):
+for i in range(100000):
     GPIO.output(19, GPIO.HIGH)
     GPIO.output(19, GPIO.LOW)
 GPIO.cleanup()
