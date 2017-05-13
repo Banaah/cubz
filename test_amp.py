cadd10a97a15b03c19d0b7d5afee2b5d7822cc7e
Banaah <nouvel.quentin@gmail.com>
2017-05-13 18:24:25 +0200

    scs

--- test_amp.py
+++ test_amp.py
@@ -6,7 +6,7 @@ for i in led:
     GPIO.setup(i, GPIO.OUT)
 #GPIO.output(26, GPIO.HIGH)
 GPIO.output(17, GPIO.LOW)
-for i in range(100000):
+for i in range(1000000):
     GPIO.output(19, GPIO.HIGH)
     GPIO.output(19, GPIO.LOW)
 GPIO.cleanup()
